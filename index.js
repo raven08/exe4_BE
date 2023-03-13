b780e74cf1fd6eb4bbe144378a77e64a06e42198
raven08 <vickyravensky@gmail.com>
2023-03-13 19:28:35 +0800

    upload file

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const members = require('./members');
+const getUsers = require('./users');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('This is the home page');
+});
+app.get('/about', (req, res) => {
+    const date = new Date().toISOString();
+    res.json({
+        status: 'success',
+        message: 'response success',
+        description: 'Exercise #03',
+        date: date,
+        data: members
+    })
+});
+app.get('/users', async (req, res) => {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(await getUsers()));
+  });
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+})
---
+++ members.js
@@ -0,0 +1,35 @@
+const members = [
+    {
+        nama: "Vicky Ravensky Pati Ani",
+        id: "S2200285",
+        nim: 12345,
+        noTlp: 081239443436
+    },
+    {
+        nama: "Prety Marchela Lamalo",
+        id: "S2200285",
+        nim: 12345,
+        noTlp: 081239443436
+    },
+    {
+        nama: "Frances Rully Yong",
+        id: "S2200285",
+        nim: 12345,
+        noTlp: 081239443436
+    },
+    {
+        nama: "Joseph Janone Tiwouw",
+        id: "S2200285",
+        nim: 12345,
+        noTlp: 081239443436
+    },
+    {
+        nama: "Cherlita Veleri Patuli",
+        id: "S2200285",
+        nim: 12345,
+        noTlp: 081239443436
+    },
+
+  ];
+  
+  module.exports = members;
